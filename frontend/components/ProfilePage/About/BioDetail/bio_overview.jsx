0e6c5f091cd11ad99e5eaa29be23c53ee4b0d915
Tulen <juaustria@gmail.com>
2017-07-21 13:33:23 -0700

    finish about bio overview layout

--- frontend/components/ProfilePage/About/BioDetail/bio_overview.jsx
+++ frontend/components/ProfilePage/About/BioDetail/bio_overview.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BioOverviewItem from './bio_overview_item'
 
 class BioOverview extends React.Component {
   render() {
@@ -7,22 +8,34 @@ class BioOverview extends React.Component {
         <div className="bio-overview-left">
           <ul>
             <li className="bio-overview-item">
-              Bio overview item
+              <BioOverviewItem />
             </li>
             <li className="bio-overview-item">
-              Bio overview item
+              <BioOverviewItem />
             </li>
             <li className="bio-overview-item">
-              Bio overview item
+              <BioOverviewItem />
             </li>
             <li className="bio-overview-item">
-              Bio overview item
+              <BioOverviewItem />
             </li>
           </ul>
         </div>
         <div className="bio-overview-right">
           <ul>
-            <li> Details </li>
+            <li>
+              <i className="fa fa-building"></i>
+              <p> Works at </p>
+            </li>
+            <li>
+              <i className="fa fa-home"></i>
+              <p> Hometown </p>
+            </li>
+            <li>
+              <i className="fa fa-birthday-cake"></i>
+              <p> Birthday </p>
+            </li>
+
           </ul>
         </div>
       </div>
---
+++ frontend/components/ProfilePage/About/BioDetail/bio_overview_item.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class BioOverviewItem extends React.Component {
+  render() {
+    return (
+      <div className="boi-container">
+        <div> <i className="fa fa-plus"> </i> </div>
+        Add a bio detail
+      </div>
+    )
+  }
+}
+
+export default BioOverviewItem
