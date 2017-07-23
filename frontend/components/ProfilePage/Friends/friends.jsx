2ca9a071a564245285413080044e6241a4a68340
Tulen <juaustria@gmail.com>
2017-07-22 18:43:14 -0700

    add friend request button, change navbar position to fixed

--- frontend/components/ProfilePage/Friends/friends.jsx
+++ frontend/components/ProfilePage/Friends/friends.jsx
@@ -5,10 +5,13 @@ class Friends extends React.Component {
   render() {
     return (
       <div id="friends-container">
-        <div className="detail-header-container">
+        <div className="detail-header-container friends-header">
           <div className="detail-header">
             <i className="fa fa-group"> </i>
             <h4> Friends </h4>
+          </div>
+          <div className="detail-header-buttons">
+            <button> Friend Requests </button>
           </div>
         </div>
         <FriendsGrid />
