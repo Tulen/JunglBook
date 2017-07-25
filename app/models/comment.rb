9edeb2073b5341bc1956b4359f1f4f695b7f2d4b
Tulen <juaustria@gmail.com>
2017-07-24 22:56:55 -0700

    fix comment.rb commas

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,22 +2,22 @@ class Comment < ApplicationRecord
   validates :author, :parent, :post, :body, presence: true
   validates :author, :parent, :post, uniqueness: true
 
-  belongs_to :author
+  belongs_to :author,
     primary_key: :id,
     foreign_key: :author_id,
     class_name: :User
 
-  belongs_to :post
+  belongs_to :post,
     primary_key: :id,
     foreign_key: :post_id,
     class_name: :Post
 
-  belongs_to :parent
+  belongs_to :parent,
     primary_key: :id,
     foreign_key: :parent_id,
     class_name: :Comment
 
-  has_many :children
+  has_many :children,
     primary_key: :id,
     foreign_key: :parent_id,
     class_name: :Comment
