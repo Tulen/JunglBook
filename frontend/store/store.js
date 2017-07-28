4b63bcf89ed8fb7c3d6a76548f093fd78c1a03eb
Tulen <juaustria@gmail.com>
2017-07-28 14:50:18 -0700

    remove redux-loger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
 import RootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
 const configureStore = (preloadedState = {}) => (
-  createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore
