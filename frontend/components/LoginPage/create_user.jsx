4d7d9b65e10acb96d9b4488d0bdadddf47e0891c
Tulen <juaustria@gmail.com>
2017-08-16 18:11:29 -0700

    Readd source attribution for email validation regex pattern

--- frontend/components/LoginPage/create_user.jsx
+++ frontend/components/LoginPage/create_user.jsx
@@ -58,6 +58,7 @@ class CreateUser extends React.Component {
     }
 
     function validateEmail(email) {
+      //Regex pattern attributed to https://stackoverflow.com/questions/46155/how-to-validate-email-address-in-javascript
       let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return emailRegex.test(email);
     }
